6b03f1b4b80e7926790be53efe51e1d3fcb41967
prajwaldpepam <prajwal_dp@epam.com>
2024-10-23 17:37:12 +0530

    initialised the project

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+
+
+app.use("/homePage", (req, res) => {
+    res.send("Hi, this is the home page");
+});
+
+app.use("/aboutPage", (req, res) => {
+    res.send("Hi, this is the about page");
+});
+
+app.use("/login", (req, res) => {
+    res.send("Hi, this is the login page");
+});
+
+app.use("/", (req, res) => {
+    res.send("Hi, this is a new project");
+});
+
+app.listen(3000, () => {
+    console.log("Server is started...!");
+});
